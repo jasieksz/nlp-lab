ab2c9f4b4f6f60db7a51119fa091d7aee298dc14
Jasiek Sznajd <jasiek.sznajd@gmail.com>
2019-03-29 16:09:18 +0100

    lab3: only levenshtein left

--- lab3/vector-analyzer.py
+++ lab3/vector-analyzer.py
@@ -1,13 +1,16 @@
 #%%
 import os
 import numpy as np
+import re
+import seaborn as sns
+import pandas as pd
+from collections import Counter
+from functools import reduce
 from elasticsearch import Elasticsearch
 from elasticsearch.client import IndicesClient
 
-#%%
-es = Elasticsearch()
-
 #%% ES index for storing acts, ES analyzer for Polish texts:
+es = Elasticsearch()
 es.indices.create(
     index="my_index",
     body={
@@ -38,9 +41,9 @@ es.indices.create(
                     "text": {
                         "type": "text",
                         "analyzer": "my_analyzer",
-                        "term_vector": "with_positions_offsets_payloads"
+                        "term_vector": "with_positions_offsets_payloads",
                     }
-                }
+                }                
             }
         }
     }
@@ -54,22 +57,35 @@ for filename in os.listdir(resource_path):
         es.create("my_index", "legislation", filename, {"text": legislation})
 
 #%% 1 Find terms (tokens) that are present in the document, and their count
-x = es.termvectors(index="my_index",
-    doc_type="legislation",
-    id="1994_395.txt")["term_vectors"]
-x["text"]["terms"].keys()
+body = {"term_statistics" : "true"} # , "filter" : {"min_word_length" : 2}
+def get_term_freq(id: str):
+    x = es.termvectors(index="my_index", doc_type="legislation", id=id, body=body)["term_vectors"]
+    return Counter(dict([(k, v["term_freq"]) for k,v in x["text"]["terms"].items() if not re.match(r'\d+', k)]))
 
 #%% 2 Aggregate the result to obtain one global frequency list
+term_freq = reduce(lambda x,y : x + y, [get_term_freq(f_id) for f_id in os.listdir(resource_path)])
 
 #%% 3 Filter the list to keep terms that contain only letters and have at least 2 of them
+term_freq = Counter(dict(filter(lambda x: len(x[0]) > 1, term_freq.most_common())))
 
 #%% 4 Plot, log scale, rank vs occurences
+rank, freq = zip(*enumerate(term_freq.most_common()))
+rank = list(map(lambda x: x+1, rank))
+freq = list(map(lambda x: int(x), np.array(freq)[:,1]))
+
+sns.set_context("notebook")
+g = sns.lineplot(x=rank, y=freq, color='coral', lw=2.75)
+g.set(yscale="log")
 
 #%% 5 Find all words not in dictionary: "polimorfologik"
-
+df = pd.read_csv('resources/polimorfologik-2.1/polimorfologik-2.1.csv', delimiter=';')
+forms = set(map(lambda x: re.sub(r'-', '', x).lower(), df['form'].unique()))
+missing = [x for x in term_freq.most_common() if not x[0] in forms]
 
 #%% 6 30 words, with highest ranks that do not belong to the dict
+missing[:30]
 
 #%% 7 Find 30 words with 3 occurrences that do not belong to the dictionary
+list(map(lambda x: (re.sub(r'\xad', '', x[0]), x[1]), filter(lambda x: x[1] == 3, missing)))
 
 #%% 8 Use Levenshtein distance and the frequency list, to determine correction
