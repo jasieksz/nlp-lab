e98df4359df024148f85f37c92317e45f58355a9
Jasiek Sznajd <jasiek.sznajd@gmail.com>
2019-04-08 10:22:15 +0200

    lab4: pmi

--- lab4/multiword-ranking.py
+++ lab4/multiword-ranking.py
@@ -1,11 +1,14 @@
 #%%
 import numpy as np
 import os
+import re
 import seaborn as sns
 from collections import Counter
 from functools import reduce, partial
 from elasticsearch import Elasticsearch
 from elasticsearch.client import IndicesClient
+from lab1 import shapers
+from typing import List, Tuple
 
 #%% ES index for storing acts, ES analyzer for Polish texts:
 es = Elasticsearch()
@@ -29,7 +32,6 @@ es.indices.create(
                     "text": {
                         "type": "text",
                         "analyzer": "my_analyzer",
-                        "term_vector": "with_positions_offsets_payloads"
                     }
                 }                
             }
@@ -46,21 +48,43 @@ analyze = lambda text: es.indices.analyze("my_index", body(text))
 
 get_shingles = lambda text: list(map(lambda x: x['token'],
                                 filter(lambda x: x['type'] == 'shingle',
-                                analyze(text)['tokens'])))
+                                text)))
+
+get_tokens = lambda text: list(map(lambda x: x['token'],
+                                filter(lambda x: x['type'] == '<ALPHANUM>',
+                                text)))
 
 #%% 1.1 Compute bigram counts in the corpora
 resource_path = 'resources/ustawy'
-shingles = []
+doc_shingles: List[List[str]] = []
+doc_tokens: List[List[str]] = []
 for filename in os.listdir(resource_path):
     with open(resource_path + '/' + filename, 'r') as document:
         legislation = document.read()
-        shingles.append(get_shingles(legislation))
+        doc = analyze(legislation)['tokens']
+        doc_shingles.append(get_shingles(doc))
+        doc_tokens.append(get_tokens(doc))
+
+shingles: List[str] = shapers.flatten(doc_shingles)
+tokens: List[str] = shapers.flatten(doc_tokens)
+
+#%% 
+text_only = lambda x: not re.search(r'\d+', x)
+make_tuple = lambda x: tuple(x.split(' '))
 
 #%% 1.2 Filter shingles - text only, lowercase, no punctuation
+shingle_freq = Counter(list(map(make_tuple, filter(text_only, shingles))))
+tokens_freq = Counter(list(filter(text_only, tokens)))
 
 #%% 2. Pointwise Mutal Information score
+p_t = lambda token: tokens_freq[token] / len(tokens_freq)
+p_s = lambda shingle: shingle_freq[shingle] / len(tokens_freq)
+pmi = lambda x, y: np.log(p_s((x, y)) / (p_t(x) * p_t(y)))
+
+pmis = [(s, pmi(s[0], s[1])) for s in shingle_freq.keys()]
 
 #%% 3. 30 top from ex.2
+sorted(pmis, key=lambda e: e[1], reverse=True)[:10]
 
 #%% 4. Log Likelihood Ration score
 
